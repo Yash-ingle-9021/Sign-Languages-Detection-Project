15768ab26ca413cf86be47e701cbddbba0b3a28e
Yash-ingle-9021 <codebyyash0803@gmail.com>
2023-12-15 01:40:41 -0800

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguages',
+    version= '0.0.0',
+    author= 'Yash Ingle',
+    author_email= 'codebyyash0803@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
